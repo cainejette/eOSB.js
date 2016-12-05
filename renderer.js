93f2005797f2491e191b9b1859a33af0180057a3
Caine Jette <caine@attackpattern.com>
2016-12-05 00:02:32 -0800

    Allow next round selection once current round is over.

--- renderer.js
+++ renderer.js
@@ -1,13 +1,9 @@
 'use strict';
 
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
-var parser = require('xml2js');
-var fs = require('fs');
-var path = require('path');
-var PDFJS = require('pdfjs-dist-for-node');
+const parser = require('xml2js');
+const fs = require('fs');
+const path = require('path');
+const PDFJS = require('pdfjs-dist-for-node');
 
 let round;
 let round_number = 1;
@@ -32,22 +28,41 @@ document.querySelector('#tcq_a_solution').addEventListener('click', () => open_t
 document.querySelector('#tcq_b_solution').addEventListener('click', () => open_tcq('b_solutions'));
 document.querySelector('#hide_tcq').addEventListener('click', () => hide_tcq());
 document.querySelector('#prev').addEventListener('click', () => update_question(--question_number));
-document.querySelector('#next_question').addEventListener('click', () => update_question(++question_number));
+document.querySelector('#next_question').addEventListener('click', () => next_tossup());
 document.querySelector('#next_tossup').addEventListener('click', () => update_question(++question_number));
+document.querySelector('#choose_another').addEventListener('click', () => choose_round());
 
 document.querySelector('#question').setAttribute('style', 'display: none');
+document.querySelector('#inputs').setAttribute('style', 'display: none');
+document.querySelector('#round_over').setAttribute('style', 'display: none');
+
+function choose_round() {
+  document.querySelector('#round_selection').setAttribute('style', 'display: visible');
+  document.querySelector('#question').setAttribute('style', 'display: none');
+  document.querySelector('#inputs').setAttribute('style', 'display: none');
+  document.querySelector('#round_over').setAttribute('style', 'display: none');
+}
 
 function open_round(new_round_number) {
   round_number = new_round_number;
   fs.readFile(path.join(__dirname, 'questions', `/round_${round_number}.xml`), function(err, data) {
     parser.parseString(data, function (err, result) {
       round = result.Round.Questions[0];
-
       document.querySelector('#question').setAttribute('style', 'display: visible');
+      document.querySelector('#inputs').setAttribute('style', 'display: visible');
       document.querySelector('#round_selection').setAttribute('style', 'display: none');
       update_question(0);
     });
   });
+}
+
+function next_tossup() {
+  // advance two questions if on tossup
+  if (question_number % 2 == 0) {
+    update_question(question_number + 2);
+  } else {
+    update_question(question_number + 1);
+  }
 }
 
 function update_question(new_question_number) {
@@ -55,26 +70,30 @@ function update_question(new_question_number) {
 
   update_buttons(question_number);
   if (question_number == 20 && !opened_tcq) {
-    // show tcq
-  }
-
-  question = round.Question[question_number];
-  document.querySelector('#question_number').textContent = question.QuestionPair[0];
-
-  if (question.QuestionFormat[0] === 'Multiple Choice') {
-    document.querySelector('#question_choices').setAttribute('style', 'display: visible');
-    document.querySelector('#option_w').textContent = question.AnswerW[0];
-    document.querySelector('#option_x').textContent = question.AnswerX[0];
-    document.querySelector('#option_y').textContent = question.AnswerY[0];
-    document.querySelector('#option_z').textContent = question.AnswerZ[0];
+    document.querySelector('#tcq').setAttribute('style', 'display: visible');
   } else {
-    document.querySelector('#question_choices').setAttribute('style', 'display: none');
+    document.querySelector('#tcq').setAttribute('style', 'display: none');
   }
 
-  document.querySelector('#question_format').textContent = question.QuestionFormat[0];
-  document.querySelector('#question_type').textContent = question.QuestionType[0];
-  document.querySelector('#question_text').textContent = question.QuestionText[0];
-  document.querySelector('#question_answer').textContent = question.CorrectAnswer[0];
+  if (question_number < 40) {
+    question = round.Question[question_number];
+    document.querySelector('#question_number').textContent = question.QuestionPair[0];
+
+    if (question.QuestionFormat[0] === 'Multiple Choice') {
+      document.querySelector('#question_choices').setAttribute('style', 'display: visible');
+      document.querySelector('#option_w').textContent = question.AnswerW[0];
+      document.querySelector('#option_x').textContent = question.AnswerX[0];
+      document.querySelector('#option_y').textContent = question.AnswerY[0];
+      document.querySelector('#option_z').textContent = question.AnswerZ[0];
+    } else {
+      document.querySelector('#question_choices').setAttribute('style', 'display: none');
+    }
+
+    document.querySelector('#question_format').textContent = question.QuestionFormat[0];
+    document.querySelector('#question_type').textContent = question.QuestionType[0];
+    document.querySelector('#question_text').textContent = question.QuestionText[0];
+    document.querySelector('#question_answer').textContent = question.CorrectAnswer[0];
+  }
 }
 
 function update_buttons(question_number) {
@@ -87,10 +106,14 @@ function update_buttons(question_number) {
   }
 
   // disable forward buttons on last question
-  if (question_number == 39) {
+  if (question_number == 40) {
+    document.querySelector('#round_over').setAttribute('style', 'display: visible');
+    document.querySelector('#question').setAttribute('style', 'display: none');
     document.querySelector('#next_question').setAttribute('disabled', 'disabled');
     document.querySelector('#next_tossup').setAttribute('disabled', 'disabled');
   } else {
+    document.querySelector('#round_over').setAttribute('style', 'display: none')
+    document.querySelector('#question').setAttribute('style', 'display: visible');
     document.querySelector('#next_question').removeAttribute('disabled');
     document.querySelector('#next_tossup').removeAttribute('disabled');
   }
