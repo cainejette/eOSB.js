5c7f408740512b7f0b69a0bab17bd45850a616cc
Caine Jette <caine@attackpattern.com>
2016-12-08 18:12:17 -0800

    Center text in round selection buttons.

--- renderer.js
+++ renderer.js
@@ -26,20 +26,22 @@ function build_round_buttons() {
   create_open_round_button();
 
   rounds.forEach(round => {
-    var button = document.createElement('li');
-    button.setAttribute('class', 'round_button col-xs-4 ' + (round.opened ? 'opened' : ''));
+    var button = document.createElement('button');
+    button.setAttribute('class', 'round_button col-xs-4 btn btn-primary' + (round.opened ? ' opened' : ''));
+    button.textContent = round.name;
 
     var input = document.createElement('input');
     input.setAttribute('type', 'radio')
     input.setAttribute('name', 'rounds');
     input.id = round.file.split('.')[0];
     input.addEventListener('click', () => enable_open_round_button());
+    input.textContent = round.name;
     button.appendChild(input);
 
     var label = document.createElement('label');
     label.setAttribute('for', round.file.split('.')[0]);
     label.textContent = round.name;
-    button.appendChild(label);
+    // button.appendChild(label);
     
     document.querySelector('#rounds').appendChild(button);
   });
