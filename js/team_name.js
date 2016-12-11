7c90661049854c31dd6361e86da73addc31eafbe
Caine Jette <caine@attackpattern.com>
2016-12-11 13:33:13 -0800

    Plumb through values of team names from dialog to main

--- js/team_name.js
+++ js/team_name.js
@@ -5,7 +5,10 @@ document.querySelector('#cancel_button').addEventListener('click', () => {
 });
 
 document.querySelector('#submit_button').addEventListener('click', () => {
-  ipcRenderer.send('close_team_name_dialog', 'caine', 'vivian');
+  ipcRenderer.send(
+    'close_team_name_dialog',
+    document.querySelector('#team_a_name').value, 
+    document.querySelector('#team_b_name').value);
 });
 
 
