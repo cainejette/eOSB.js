4b4c67fd28da53808b7d71dd99774777869eeb34
Caine Jette <caine@attackpattern.com>
2017-02-27 22:09:39 -0800

    Add smoother interactivity to login page.

--- eOSB.js/app/components/login/loginController.js
+++ eOSB.js/app/components/login/loginController.js
@@ -1,18 +1,18 @@
-(function () {
-    'use strict';
-    angular.module('app')
-        .controller('loginController', ['$location', '$scope', LoginController]);
-    
-    function LoginController($location, $scope) {
-        $scope.password = '';
-
-        $scope.login = function() {
-            if ($scope.password = 'hi') {
+'use strict';
+angular.module('app').controller('loginController',
+    ['$location', '$scope', function ($location, $scope) {
+        $scope.login = function () {
+            if ($scope.password == 'hi') {
                 $location.path('/rounds');
             }
             else {
-
+                $scope.invalid_password = true;
+                $scope.password = "";
             }
         }
-    }
-})();
+
+        $scope.clear = function () {
+            $scope.invalid_password = false;
+        }
+    }]
+);
