82c5105fa23505180fe1df37271e96c05c7af687
Caine Jette <caine@attackpattern.com>
2017-03-02 22:09:04 -0800

    Add scorekeeping/timekeeping checkboxes and next button to rounds screen.

--- eOSB.js/app/components/rounds/roundsController.js
+++ eOSB.js/app/components/rounds/roundsController.js
@@ -2,5 +2,8 @@
 angular.module('app').controller('roundsController', 
     ['$location', '$scope', function($location, $scope) {
         $scope.rounds = require('./app/questions/info.json');
+
+        $scope.useScorekeeping = false;
+        $scope.useTimekeeping = false;
     }]
 );
