f18ae5d6a5597fbd9d0945ec8c0eb1716d726a5f
Caine Jette <caine@attackpattern.com>
2017-03-02 22:32:30 -0800

    Hook up click handler, style buttons.

--- eOSB.js/app/components/rounds/roundsController.js
+++ eOSB.js/app/components/rounds/roundsController.js
@@ -2,8 +2,19 @@
 angular.module('app').controller('roundsController', 
     ['$location', '$scope', function($location, $scope) {
         $scope.rounds = require('./app/questions/info.json');
+        $scope.selectedRound = undefined;
 
         $scope.useScorekeeping = false;
         $scope.useTimekeeping = false;
+
+        $scope.check = function(round) {
+            $scope.selectedRound = round;
+        }
+
+        $scope.openRound = function() {
+            console.log('use scorekeeper: ' + $scope.useScorekeeping);
+            console.log('use timekeeper: ' + $scope.useTimekeeping);
+            console.log('open round: ' + $scope.selectedRound.name);
+        }
     }]
 );
