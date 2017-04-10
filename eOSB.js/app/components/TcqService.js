d2ebb5cf878649e9980f04008f8704ca29ae3a83
Caine Jette <caine@attackpattern.com>
2017-04-09 18:50:22 -0700

    More plumbing for TCQ window

--- eOSB.js/app/components/TcqService.js
+++ eOSB.js/app/components/TcqService.js
@@ -7,18 +7,49 @@ const path = require('path');
 
 angular.module('app').service('TcqService', function() {
   
-  this.openTcq = function(round, index) {
-    console.log('trying to open pdf...', round, index);
-    
-    let win = new BrowserWindow({width: 800, height: 600})
+  let namesForIndices = ['a', 'b', 'a_solutions', 'b_solutions'];
 
-    win.loadURL(url.format({
+  this.openTcq = function(round, index) {
+    console.log('trying to open pdf...', round, namesForIndices[index]);
+    
+    let tcqWindow = new BrowserWindow({width: 800, height: 600})
+
+    tcqWindow.loadURL(url.format({
       pathname: path.join(__dirname, 'app', 'components', 'tcq.html'),
       protocol: 'file:',
       slashes: true
     }));
-    // win.loadURL('./app/components/tcq.html');
-    win.webContents.openDevTools();
+
+    var tcqPath = `${__dirname}/app/questions/tcq_${round}_${namesForIndices[index]}.pdf`;
+    console.log('opening: ', tcqPath);
+    
+    PDFJS.disableWorker = true;
+
+    PDFJS.workerSrc = `${__dirname}/../node_modules/pdfjs-dist/build/pdf.worker.js`;
+    // PDFJS.workerSrc = '';
+
+    PDFJS.getDocument(tcqPath).then(function getPdfHelloWorld(pdf) {
+      pdf.getPage(1).then(function getPageHelloWorld(page) {
+        var scale = 1.5;
+        var viewport = page.getViewport(scale);
+
+        console.dir(page);
+        console.dir(document);
+        var canvas = document.querySelector('#tcq');
+        console.dir(canvas);
+        var context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        var renderContext = {
+          canvasContext: context,
+          viewport: viewport
+        };
+        page.render(renderContext);
+      });
+    });
+
+    tcqWindow.webContents.openDevTools();
 
   };
 
